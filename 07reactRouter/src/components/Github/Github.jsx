eda56c24558e76e96296ba165aa5ca168973d868
harshad <harshadsurve2003@gmail.com>
2024-07-15 16:37:43 +0530

    reactRouter

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+function Github() {
+    const data =useLoaderData()
+    // const [data,setData]=useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then((res)=>res.json())
+    //  .then((data)=>{
+    //     console.log(data);
+    //     setData(data)
+    // })
+    // },[])
+    return (
+        <div className='text-center m-4 bg-gray-500 text-white p-4 text-3xl'>
+        Github followers: {data.followers}
+        <img
+          src={data.avatar_url}
+          alt="Git picture"
+          width={300}
+          style={{ display: 'block', margin: '0 auto' }}
+        />
+      </div>
+    )
+}
+
+export default Github
+
+export const githubInfoLoader = async ()=>{
+    const res = await fetch('https://api.github.com/users/hiteshchoudhary')
+    const data = await res.json()
+    return(data)
+}
