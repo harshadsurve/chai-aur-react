abb2b0078d1bde57e184e4d0b4c29f5beda92bed
harshad <harshadsurve2003@gmail.com>
2024-07-18 19:58:24 +0530

    Redux

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -17,18 +17,16 @@ export const todoslice = createSlice({
         text: action.payload
       };
       state.todos.push(todo);
-      return state;
     },
     removeTodo: (state, action) => {
       state.todos = state.todos.filter((todo) => todo.id !== action.payload);
-      return state;
     },
     editTodo: (state, action) => {
-      const todo = state.todos.find((todo) => todo.id === action.payload.id);
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id === id);
       if (todo) {
-        todo.text = action.payload.text;
+        todo.text = text;
       }
-      return state;
     }
   }
 });
